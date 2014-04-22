38a4547bf15ec632fb6f4dce739d9b019540e8bd
Owain Lleweellyn <owain.llew@gmail.com>
2014-04-22 10:42:53 -0400

    added helper functions

--- dev/js/modules/module.js
+++ dev/js/modules/module.js
@@ -1,32 +1,39 @@
-"use strict"
+(function(window, undefined) {
+	"use strict";
 
-/**
-* This is my first module/component/widget/plugin.
-*
-* @class module
-* @constructor
-*/
-
-var module = {
-
+	var module = module || null;
+	
 	/**
-	* My module description.  Like other pieces of your comment blocks, 
-	* this can span multiple lines.
+	* This is my first module/component/widget/plugin.
 	*
-	* @method AddTwoNumbers
-	* @param {number} First number
-	* @param {number} Second Number
-	* @param {Function} Returns the value of first number added to second number
-	* @return {Number} Returns Number or NAN
+	* @class module
+	* @constructor
 	*/
 
-	AddTwoNumbers: function(a,b) {
-		return (a+b);
-	}
-};
+	var moduleFeature = {
 
-if(module) {
-	module.exports = module;
-} else {
-	window.module = module;
-}
+		/**
+		* My module description.  Like other pieces of your comment blocks, 
+		* this can span multiple lines.
+		*
+		* @method AddTwoNumbers
+		* @param {number} First number
+		* @param {number} Second Number
+		* @param {Function} Returns the value of first number added to second number
+		* @return {Number} Returns Number or NAN
+		*/
+
+		AddTwoNumbers: function(a,b) {
+			return (a+b);
+		}
+	};
+
+	// Check if we have Browserify
+	if(module) {
+		module.exports = moduleFeature;
+	} else {
+		window.moduleFeature = moduleFeature;
+	}
+
+
+})(window, undefined);
--- dev/js/utilities/helpers.js
+++ dev/js/utilities/helpers.js
@@ -1,11 +1,83 @@
-module.exports = {
-	isMobile: function() {
-		if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-			return true;
-		} else {
-			return false;
-		}
-	},
-	isDesktop: function() {
+(function(window, undefined) {
+	"use strict";
+
+	var module = module || null;
+	/**
+	* This is our helpers singleton. Anything that can be shared across modules can be used here.
+	*
+	* @class module
+	* @constructor
+	*/
+
+	var helpers = {
+
+		/**
+		* My isMobile description.  Like other pieces of your comment blocks, 
+		* this can span multiple lines.
+		*
+		* @method isMobile
+		* @return {Boolean} Returns true or false
+		*/
+
+		
+		isMobile: function() {
+			if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+
+		/**
+		* isIpad, detects iPad in the browser by checking userAgent
+		*
+		* @method isIpad
+		* @return {Boolean} Returns true or false
+		*/
+
+		isIpad: function() {
+			return navigator.userAgent.indexOf('iPad') !== -1;
+		}, 
+
+		/**
+		* isTouch, detects if device is touch enabled in the browser by checking userAgent
+		*
+		* @method isTouch
+		* @return {Boolean} Returns true or false
+		*/
+
+		isTouch: function() {
+			return (this.eventClick === "touchend")
+		},
+
+		/**
+		* My isDesktop description.  Like other pieces of your comment blocks, 
+		* this can span multiple lines.
+		*
+		* @method isDesktop
+		* @return {Boolean} Returns true or false
+		*/
+
+		isDesktop: function() {
+			//TODO
+			return; 
+		},
+
+    	eventStart: (typeof window.ontouchstart === "undefined") ? 'mousedown' : 'touchstart',
+
+		eventEnd: (typeof window.ontouchstart === "undefined") ? 'mouseup' : 'touchend',
+
+    	eventMove: (typeof window.ontouchstart === "undefined") ? 'mousemove' : 'touchmove',
+
+    	eventClick: (typeof window.ontouchstart === "undefined") ? 'click' : 'touchend'
+	};
+
+	// Check if we have Browserify
+	if(module) {
+		module.exports = helpers;
+	} else {
+		window.helpers = helpers;
 	}
-};
+
+
+})(window, undefined);
